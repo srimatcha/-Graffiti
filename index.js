80fa1fcf5d24d0244459f2d860f12dfc59da18f1
srimatcha <loupadoowah@gmail.com>
2022-10-18 19:05:35 -0700

    changees

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Select some elements...
+
+let header = document.queryselector("#page-header");
+header.style.textAlign = "left"
+
+
+let dogImages = document.querySelectorAll(".dog-image")
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = "50px"
+}
+
+
+let dogPar = document.querySelectorAll(".dog-description")
+for (let i = 0; i < dogPar.length; i++) {
+    dogPar[i].style.fontFamily = "Brush Script MT"
+}
